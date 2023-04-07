6621e8d9a661dd391af93de67e6fefecb5007437
Aleksandra Smela <aleksandra.smela@gmail.com>
2023-04-07 20:26:26 +0200

    znajdowanie argumentów zaimplementowane (mam nadzieję)

--- calc.asm
+++ calc.asm
@@ -39,7 +39,19 @@ data1 segment
 
     err1    db "Blad danych wejsciowych!$"
 
-    buff1 db 50, ?, 55 dup("$") 
+    buff1 db 50, ?, 55 dup("$") ;bufor na znaki wprowadzone przez uzytkownika
+
+    arg1_start  db 0
+    arg1_end    db 0
+    arg1        db 0
+    op_start    db 0
+    op_end      db 0
+    oper        db 0
+    arg2_start  db 0
+    arg2_end    db 0
+    arg2        db 0
+
+
 
 data1 ends
 
@@ -60,15 +72,82 @@ start1:
     mov     ah, 0ah
     int     21h
 
+    mov     ax, seg data1; znalezienie pierwszego arumentu
+    mov     ds, ax
+    mov     cl, byte ptr ds:[buff1+1] ;zapisanie rozmiaru bufora danych wpisanych przez użytkownika
+    mov     ch, 0 ;zerowanie licznika do iteracji
+    mov     bx, 2 ;ustawienie wskaznika na pierwszy znak w buforze
+
+    get_arg1_start:
+        mov     byte ptr ds:[arg1_start], 0
+    
+    find_arg1_end:
+        mov     dh, byte ptr ds:[buff1+bx] ;zaladuj znak z bufora
+
+        cmp     cl, ch ;jesli dotarl do konca stringa to podano za malo argumentow
+        je      exception1 ;wyrzuc wyjatek
+
+        cmp     dh, 32 ;porownaj czy znak jest spacja, 32-kod ASCII spacji
+        je      get_arg1_end ;jesli tak to zapisz koniec argumentu
+
+        inc     bx ;przesun wskaznik na kolejny znak
+        inc     ch ;inkrementuj licznik
+    jmp find_arg1_end
+
+    get_arg1_end:
+        dec     ch ;przesun ch na pierwszy znak przed spacja
+        mov     byte ptr ds:[arg1_end], ch
+        inc     ch
+
+    ;get_op_start:
+    inc     ch ;przejdz do pierwszego znaku po spacji
+    inc     bx ;inkrementuj wskaźnik na kolejny znak
+    mov     byte ptr ds:[op_start], ch
+
+    find_op_end:
+        mov     dh, byte ptr ds:[buff1+bx] ;zaladuj znak z bufora
+
+        cmp     cl, ch ;jesli dotarl do konca stringa to podano za malo argumentow
+        je      exception1 ;wyrzuc wyjatek
+
+        cmp     dh, 32 ;porownaj czy znak jest spacja, 32-kod ASCII spacji
+        je      get_op_end ;jesli tak to zapisz koniec argumentu
+        
+        inc     bx ;przesun wskaznik na kolejny znak
+        inc     ch ;inkrementuj licznik petli
+    jmp find_op_end
+
+    get_op_end:
+        cmp     cl, ch ;jesli dotarl do konca stringa to podano za malo argumentow
+        je      exception1 ;wyrzuc wyjatek
+        dec     ch ;przesun ch na pierwszy znak przed spacja
+        mov     byte ptr ds:[op_end], ch
+        inc     ch
+    
+    ;get_arg2_start:
+    inc     ch ;przejdz do pierwszej pozycji argumentu po spacji
+    inc     bx ;inkrementuj wskaźnik na kolejny znak
+    cmp     cl, ch ;jesli dotarl do konca stringa to podano za malo argumentow
+    je      exception1 ;wyrzuc wyjatek
+    mov     byte ptr ds:[arg2_start], ch
+
+    ;get_arg2_end:
+    mov     byte ptr ds:[arg2_end], cl
+    
+    
+
+
+
     call    printnline ;wypisanie tego co zostalo wczytane
     mov     dx, offset resmes
     call    print1
     mov     dx, offset buff1+2 
     call    print1
 
-	mov     al,0 ;koniec programu
-	mov     ah,4ch
-	int     21h
+    end_prog:
+        mov     al,0 ;koniec programu
+        mov     ah,4ch
+        int     21h
 
 ;============================================
 ;paramert dx - offset
@@ -78,7 +157,7 @@ start1:
         mov     ah, 9h
         int     21h
         ret
-
+;============================================
     printnline:
         mov     ax, seg data1
         mov     ds, ax 
@@ -86,6 +165,12 @@ start1:
         mov     ah, 9h
         int     21h
         ret
+;============================================
+    exception1:
+        call    printnline
+        mov     dx, offset err1
+        call    print1
+        jmp     end_prog    
 	
 
 code1 ends
